1892c8954f632d5df77570cd2e4b8a9542888b48
mrLochness350 <kachlonjonathan@gmail.com>
2021-01-26 13:54:29 +0000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-if [ "$EUID" -ne 0 ]; then
+if [ "$EUID" -ne 0 ];
+    then
     echo "please run as root"
     exit
     else
-        mkdir ~/$HOME/$USER/pcapComparer && cd ~/$HOME/$USER/pcapComparer
+        apt-get install sqlite3
+        mkdir pcapComparer 
+        cd pcapComparer
         sqlite3 pcapStorage.db
         cd 
-    fi
+        exit
 fi
