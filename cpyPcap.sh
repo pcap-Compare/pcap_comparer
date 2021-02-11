de6d53684617dce7bfa72650e73c9ce91ed96bd7
mrLochness350 <kachlonjonathan@gmail.com>
2021-02-11 15:46:12 +0200

    Add files via upload

--- cpyPcap.sh
+++ cpyPcap.sh
@@ -4,19 +4,24 @@
 
 function scpMain {
     #checks to see if a connection exists
-    #$1 = port
-    #$2 = hostname@ip
+    #$1 = hostname@ip
     wget -q --spider http://google.com
     if [ ! "$?" -eq 0 ]
     then
        echo "please connect to a network"
     else
-        echo "transferring files..."
-        scp -r "pcapFolder" $1:"/$HOME/"
-        
-        if [ ! $? -eq 0 ]
+        if [ ! -z "$1" ]
         then
-            exit
+            echo "transferring files..."
+            scp -r "pcapFolder" $1:"/$HOME/"
+        
+            if [ ! $? -eq 0 ]
+            then
+                exit
+            fi
+        else
+            echo "usage: ./cpyPcap.sh hostname@ip"
+            exit 
         fi
     fi
 }
@@ -27,10 +32,10 @@ then
     echo "please run as root"
     exit
 else
-    while [ $COUNTER -lt 3600 ]; do 
+    while [ $COUNTER -lt 3600 ]; do #change to whatever time you want **IN SECONDS**
         sleep 1
         let COUNTER+=1
-        if (( $COUNTER == 3600 )) 
+        if (( $COUNTER == 3600 )) #change to whatever time you want **IN SECONDS**
         then 
             scpMain $1 
             let COUNTER=0
